e1e0579cd5ebaaeee8ea37b3ca3cb23af16b7f7f
dannyrscott <dscott@leadnomics.com>
2014-08-18 13:56:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# grunt-lnmx-backend
+
+> Scaffold a backend application with [grunt-init][].  Includes, JSHint, mocha tests and coverage via blanket
+
+[grunt-init]: http://gruntjs.com/project-scaffolding
+
+## Installation
+If you haven't already done so, install [grunt-init][].
+
+Once grunt-init is installed, place this template in your `~/.grunt-init/` directory. It's recommended that you use git to clone this template into that directory, as follows:
+
+```
+git clone git@github.com:Lnmix/grunt-lnmx-backend.git ~/.grunt-init/grunt-lnmx-backend
+```
+
+##Usage
+
+At the command-line, cd into an empty directory, run this command and follow the prompts.
+
+```
+grunt-init grunt-lnmx-backend
+```
---
+++ template.js
@@ -0,0 +1,44 @@
+'use strict';
+
+exports.description = 'Quickly scaffold a back-end LNMX project';
+
+exports.after = 'You should now install project dependencies with _npm ' +
+'install_.  You can then hint and test your project with _grunt_. You will ' +
+'probably also want to look into building a Vagrantfile for yourself.  Someday ' +
+'I will be able to do that for you.';
+
+exports.warnOn = '*';
+
+exports.template = function(grunt, init, done) {
+	init.process({}, [
+		init.prompt('name'),
+		init.prompt('title'),
+		init.prompt('description'),
+		init.prompt('version'),
+		init.prompt('main')
+	], function(err, props) {
+		var files = init.filesToCopy(props);
+		props.projectRoot = props.main.split('/').shift();
+		grunt.file.mkdir(props.projectRoot);
+		grunt.file.mkdir('test');
+		grunt.file.mkdir('test/'+ props.projectRoot);
+		init.copyAndProcess(files, props);
+		init.writePackageJSON('package.json', {
+			name: props.name,
+			description: props.description,
+			version: props.version,
+			main: props.main,
+			devDependencies: {
+				"blanket": "^1.1.6",
+			    "grunt": "^0.4.5",
+			    "grunt-contrib-jshint": "^0.10.0",
+			    "grunt-env": "^0.4.1",
+			    "grunt-mocha-test": "^0.11.0",
+			    "mocha": "^1.21.4",
+			    "should": "^4.0.4",
+			    "travis-cov": "^0.2.5"
+			},
+		});
+		done();
+	});
+};
